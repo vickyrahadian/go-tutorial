46748f2766167d60703506b0e381ac72e41691c1
vicky rahadian <vicky.rahadian@gmail.com>
2022-06-09 09:11:51 +0700

    fixing leak

--- leetcode/TwoSum.go
+++ leetcode/TwoSum.go
@@ -3,8 +3,8 @@ package main
 func twoSum(nums []int, target int) []int {
 	indexMap := make(map[int]int)
 	for currIndex, currNum := range nums {
-		requiredIdx, isPresent := indexMap[target-currNum]
-		if isPresent {
+
+		if requiredIdx, isPresent := indexMap[target-currNum]; isPresent {
 			return []int{requiredIdx, currIndex}
 		}
 		indexMap[currNum] = currIndex
